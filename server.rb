2b4181a5d3b6cb840b909738157f7ae8b4b2c8e8
Andersen Fan <as181920@hotmail.com>
2013-03-01 16:57:00 +0800

    try get post message

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@
 require "sinatra"
 require 'active_support/all'
 require 'digest/md5'
+require 'rexml/document'
 require "yaml"
 require "logger"
 require "pry"
@@ -25,7 +26,23 @@ class Server < Sinatra::Application
   end
 
   post "/" do
-    ""
+    puts Hash.from_xml params
+    puts params
+=begin
+    request.body.rewind
+    data = request.body.read
+    receive_message =  save_message data
+
+    if receive_message.present?
+      puts "receive data #{data}"
+      auto_reply_message = reply_message(receive_message)
+      if auto_reply_message.present?
+        puts "respond with #{auto_reply_message.weixin_xml if  auto_reply_message.present?}"
+        auto_reply_message.weixin_xml
+      end
+    end
+=end
+
   end
 
   private
@@ -42,6 +59,8 @@ class Server < Sinatra::Application
     signature = [token.to_s,timestamp.to_s,nonce.to_s].sort.join("")
     Digest::SHA1.hexdigest(signature)
   end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
